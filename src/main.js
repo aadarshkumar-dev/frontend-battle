e1b30134cd7c6410ea48becbfeb6dfc7be906f61
aadarshkumar-dev <dev.aadarshkumar@gmail.com>
2026-06-26 14:51:58 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "./css/reset.css";
+import "./css/variables.css";
+import "./css/base.css";
+import "./css/layout.css";
+import "./css/components.css";
+import "./css/animations.css";
+import "./css/responsive.css";
